7789dec4e0b2aea30be2bad28dd3d80f3ef9a59c
Sebas <zhadow.franco@gmail.com>
2024-09-14 16:54:58 -0500

    add backend logic

--- app.js
+++ app.js
@@ -18,10 +18,75 @@ app.get('/', (req, res) => {
 
 // Ruta para manejar el formulario enviado
 app.post('/submit', (req, res) => {
-  const length = req.body.length;
-  console.log(req.body); // Obtener el número enviado desde el formulario
-  res.send(`Número recibido: ${length}`);
-  console.log(length); // Imprime el número en la consola del servidor
+  const type = req.body.conversionType;
+  const fromUnit = req.body.fromUnit;
+  const toUnit = req.body.toUnit;
+  const value = req.body.value;
+  
+  const conversionFactors = {
+    length: {
+      Milímetro: 1000,
+      Centímetro: 100,
+      Metro: 1,
+      Kilómetro: 0.001,
+      Pulgada: 39.3701,
+      Pie: 3.28084,
+      Yarda: 1.09361,
+      Milla: 0.000621371,
+    },
+    weigth: {
+      Miligramo: 1000,
+      Gramo: 1,
+      Kilogramo:0.001 ,
+      Onza: 0.035274,
+      Libra: 0.00220462,
+    },
+
+    temperature: {
+      Celsius:1,
+      Fahrenheit:33.8 ,
+      Kelvin: 274.15,
+    }
+
+  };
+
+
+  function convertUnit(value, fromUnit, toUnit, type) {
+    value = parseInt(value);
+    if (type === 'temperature'){
+      if (fromUnit === "Celsius" && toUnit === "Fahrenheit") {
+      const result = (value * 9/5) + 32;
+      return result;
+      } else if (fromUnit === "Fahrenheit" && toUnit === "Celsius") {
+      const result = (value - 32) * 5/9;
+      return result;
+      } else if (fromUnit === "Celsius" && toUnit === "Kelvin") {
+      const result = value + 273.15;
+      return result;
+      } else if (fromUnit === "Kelvin" && toUnit === "Celsius") {
+      const result = value - 273.15;
+      return result;
+      } else if (fromUnit === "Fahrenheit" && toUnit === "Kelvin") {
+      const result = ((value - 32) * 5/9) + 273.15;
+      return result;
+      } else if (fromUnit === "Kelvin" && toUnit === "Fahrenheit") {
+      const result = ((value - 273.15) * 9/5) + 32;
+      return result;
+      } else {
+        return 'Unidad de temperatura no válida';
+      }
+    }else {
+      const valueInStandar = value / conversionFactors[type][fromUnit];
+      console.log(valueInStandar);
+      const result = valueInStandar * conversionFactors[type][toUnit];
+      return result;
+    }
+  }
+
+  result = convertUnit(value, fromUnit, toUnit, type);
+
+  res.send(`${value} ${fromUnit}s converted to ${toUnit}s is: <br> <span class="font-bold text-[2rem]">${result}</span> `);
+
 });
 
 // Iniciar el servidor
